dd8c1e6cd48280e4914339a1afc04dac8fbae2b0
ZARTH24 <114113937+ZARTH24@users.noreply.github.com>
2023-09-23 19:18:26 +0800

    Some change

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...args) {
+    // Menggunakan filter() untuk membuat array baru yang hanya berisi elemen yang tidak ada dalam args
+    const newArray = array.filter(item => !args.includes(item));
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
